52a85324c26eb3284fa787aabfdc995d940e9b62
Cua Yang <cua.yang85@gmail.com>
2024-01-20 22:56:54 -0800

    setup done

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,29 @@
 console.log('Hello World!')
+function determineHouseholdPts () {
+    console.log(determineHouseholdPts);
+}
+
+let cardonPoints = 0;
+const numberOfPeople = 3;
+
+if (numberOfPeople === 1) {
+cardonPoints = cardonPoints + 14;
+} else if (numberOfPeople === 2) {
+    cardonPoints = cardonPoints + 12;
+} else if (numberOfPeople === 3) {
+    cardonPoints = cardonPoints + 10;
+} else if (numberOfPeople === 4) {
+    cardonPoints = cardonPoints + 8;
+} else if (numberOfPeople === 5) {
+    cardonPoints = cardonPoints + 6;
+} else if (numberOfPeople === 6) {
+    cardonPoints = cardonPoints + 4;
+} else if (numberOfPeople > 6) {
+}  cardonPoints = cardonPoints + 2
+
+console.log(`Based on the number of members in the household ${numberOfPeople} The points would be ${cardonPoints}`);
+
+//global scope
+
+determineHouseholdPts ()
+
