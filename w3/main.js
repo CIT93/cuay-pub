eac5477abebb82a38aedd5a167c54aad531439df
Cua Yang <cua.yang85@gmail.com>
2024-01-23 19:29:01 -0800

    working code

--- w3/main.js
+++ w3/main.js
@@ -29,26 +29,37 @@
 //determineHouseholdPts(3);
 //determineHouseholdPts(4);
 
+const select = document.querySelector("select");
+const para = document.querySelector("p");
+
+select.addEventListener("change", determineHouseholdPts);
+
+
 function determineHouseholdPts(sizeOfHome) {
     let lrgHome = 10;
     let medHome = 7;
     let smHome = 4;
+    let apt = 0;
   
-    if (sizeOfHome === 0) {
-      lrgHome = sizeOfHome + 10;
-    } else if (sizeOfHome === 7) {
-      medHome = sizeOfHome + 4;
-    } else if (sizeOfHome === 2) {
-      smHome = sizeOfHome + 0;
+    if (sizeOfHome === "lrgHome") {
+      para.textcontent cardonPts = cardonPts +10
+    } else if (sizeOfHome ==="medium") {
+      para.textcontent medHome = sizeOfHome + 7;
+    } else if (sizeOfHome === "small") {
+      para.textcontent smHome = sizeOfHome + 4;
+    } else if (sizeOfHome === "apartment") {
+      para.textcontent apt = sizeOfHome + 0;
     }
-    
-    // You can log the values here if needed
-    console.log("Large Home:", lrgHome);
-    console.log("Medium Home:", medHome);
-    console.log("Small Home:", smHome);
   }
   
-  let sizeOfHome = 0;
-  determineHouseholdPts(sizeOfHome);
+  console.log("Large Home:", lrgHome);
+  console.log("Medium Home:", medHome);
+  console.log("Small Home:", smHome);
+  console.log("apartment", apt);
 
-  
+  determineHouseholdPts("large");
+  determineHouseholdPts("medium");
+  determineHouseholdPts("small");
+  determineHouseholdPts("apartment");
+
+  console.log(`Based on the ${determineHouseholdPts} your Carbon Footprint will be`)
