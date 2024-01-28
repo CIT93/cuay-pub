5feb5fd63d227337d7068cfb69f5c49f49dd0489
Cua Yang <cua.yang85@gmail.com>
2024-01-28 06:46:55 -0800

    setup done

---
+++ w4/main.js
@@ -0,0 +1,72 @@
+const cfpData = [];
+
+let houseHoldSizePoints = 0;
+
+function determineHouseSizePts(size) {
+  let houseSizePoints = 0;
+  if (size === "large") {
+    houseSizePoints = 10;
+  } else if (size === "medium") {
+    houseSizePoints = 7;
+  } else if (size === "small") {
+    houseSizePoints = 4;
+  } else if (size === "apt") {
+    houseSizePoints = 2;
+  }
+
+  return houseSizePoints;
+}
+
+function determineHouseHoldPts(numberOfPeople) {
+  let houseHoldPoints = 0;
+  if (numberOfPeople === 1) {
+    houseHoldPoints = 14;
+  } else if (numberOfPeople === 2) {
+    houseHoldPoints = 12;
+  } else if (numberOfPeople === 3) {
+    houseHoldPoints = 10;
+  } else if (numberOfPeople === 4) {
+    houseHoldPoints = 8;
+  } else if (numberOfPeople === 5) {
+    houseHoldPoints = 6;
+  } else if (numberOfPeople > 6) {
+    houseHoldPoints = 2;
+  }
+  return houseHoldPoints;
+}
+
+function start(houseHoldMembers, houseHoldSize) {
+  const houseHoldPoints = determineHouseHoldPts(houseHoldMembers);
+  const houseSizePoints = determineHouseSizePts(houseHoldSize);
+  const total = houseHoldPoints + houseSizePoints;
+  cfpData.push([
+    houseHoldMembers,
+    houseHoldSize,
+    houseHoldPoints,
+    houseSizePoints,
+    total,
+  ]);
+
+}
+
+
+function displayOutput() {
+
+}
+
+
+start(5, "apt");
+start(4, "large");
+start(3, "medium");
+start(2, "small");
+start(6, "large");
+start(1, "apt");
+start(3, "large");
+start(4, "medium");
+start(5, "small");
+
+displayOutput()
+
+
+
+
