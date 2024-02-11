d3829d8aa45cd06c669c77bf285796ea6c5a8355
Cua Yang <cua.yang85@gmail.com>
2024-02-10 22:40:50 -0800

    my attempt at the code, before I review other students code

--- w6/main.js
+++ w6/main.js
@@ -1,4 +1,4 @@
-const FORM = document.getElementById("form")
+const FORM = document.getElementById("form");
 const OUTPUT = document.getElementById("output");
 const cfpData = [];
 
@@ -8,7 +8,7 @@ function determineHouseSizePts(size) {
     houseSizePoints = 10;
   } else if (size === "medium") {
     houseSizePoints = 7;
-  } else if (size === " small") {
+  } else if (size === "small") {
     houseSizePoints = 4;
   } else if (size === "apt") {
     houseSizePoints = 2;
@@ -36,27 +36,33 @@ function determineHouseHoldPts(numberOfPeople) {
 }
 
 function displayOutput() {
-  
-  for (obj of cfpData) {
-  const newH2 = document.createElement("h2");
-  newH2.textContent = `Carbon Footprint ${obj.cfpTotal}`;
-  const newH3 = document.createElement("h3");
-  newH3.textContent = `Based on number of People and the size of home`;
-  const newP = document.createElement("p");
-  newP.textContent = `This number is based on the number of people in the house of ${obj.houseM} (score: ${obj.houseMPTS})`;
-  newP.textContent += ` and a ${obj.houseS} size of home (score: ${obj.houseSPTS})`;
-  OUTPUT.appendChild(newH2);
-  OUTPUT.appendChild(newH3);
-  OUTPUT.appendChild(newP);
-}}
-  
+  OUTPUT.innerHTML = "";
 
-function start(houseHoldMembers, houseSize) {
+  for (obj of cfpData) {
+    const newH2 = document.createElement("h2");
+    newH2.textContent = `Carbon Footprint Calculator`;
+
+    const newH3 = document.createElement("h3");
+    newH3.textContent = `The Carbon FootPrint for ${obj.firstName} ${obj.lastName} is ${obj.cfpTotal}`;
+
+    const newP = document.createElement("p");
+    newP.textContent = `This number is based on the number of people in the house of ${obj.houseM} (score: ${obj.houseMPTS})`;
+    newP.textContent += ` and a ${obj.houseS} size home (score: ${obj.houseSPTS})`;
+
+    OUTPUT.appendChild(newH2);
+    OUTPUT.appendChild(newH3);
+    OUTPUT.appendChild(newP);
+  }
+}
+
+function start(firstName, lastName, houseHoldMembers, houseSize) {
   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
   const houseSizePTS = determineHouseSizePts(houseSize);
   const total = houseHoldPTS + houseSizePTS;
 
   const data = {
+    firstName: firstName,
+    lastName: lastName,
     houseM: houseHoldMembers,
     houseS: houseSize,
     houseMPTS: houseHoldPTS,
@@ -65,19 +71,28 @@ function start(houseHoldMembers, houseSize) {
   };
 
   cfpData.push(data);
-  displayOutput(data);
+  displayOutput();
 }
 
-console.log(cfpData);
-
-FORM.addEventListener(`submit`, function(e){
+FORM.addEventListener(`submit`, function (e) {
   e.preventDefault();
   const firstName = FORM.firstname.value;
   const lastName = FORM.lastname.value;
   const houseMembers = parseInt(FORM.housem.value);
   const houseSize = FORM.houseSize.value;
-  start(houseMembers, houseSize);
-  OUTPUT.innerHTML ="";
+  start(firstName, lastName, houseMembers, houseSize);
+  OUTPUT.innerHTML = "";
   displayOutput();
-  FORM.reset(); 
-})
+  FORM.reset();
+});
+
+
+
+
+
+
+
+
+
+
+
